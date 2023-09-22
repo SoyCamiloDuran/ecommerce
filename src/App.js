08983c81e95467512c34be989b1a7e7d5c4e0585
SoyCamiloDuran <SoyCamiloDuran@users.noreply.github.com>
2023-09-22 20:50:36 -0300

    user+category+productos+Frond+Busqueda+A.carrito

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from './containers/Home';
+import Error404 from './containers/errors/Error404';
+
+import Signup from './containers/auth/Signup';
+import Login from './containers/auth/Login';
+import Activate from './containers/auth/Activate';
+import ResetPassword from './containers/auth/ResetPassword';
+import ResetPasswordConfirm from './containers/auth/ResetPasswordConfirm';
+
+import Shop from './containers/Shop';
+import ProductDetail from './containers/pages/productDetail';
+
+import Search from './containers/pages/Search';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          {/* Error Display */}
+          <Route path="*" element={<Error404 />} />
+
+          {/* Home Display */}
+          <Route exact path="/" element={<Home />} />
+
+
+          {/* Authentication */}
+          <Route exact path='/signup' element={<Signup />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/activate/:uid/:token' element={<Activate />} />
+          <Route exact path='/reset_password' element={<ResetPassword />} />
+          <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
+
+          <Route exact path='/shop' element={<Shop />} />
+          <Route exact path='/product/:productId' element={<ProductDetail />} />
+          <Route exact path='/search' element={<Search />} />
+
+
+        </Routes>
+      </Router>
+    </Provider>
+
   );
 }
 
